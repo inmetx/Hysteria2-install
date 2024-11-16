a86c7eb299431695aaa467594e81788056ecc18b
0xM2C ® <38844751+inmetx@users.noreply.github.com>
2024-11-17 05:46:40 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -244,7 +244,7 @@ insthysteria(){
     fi
     ${PACKAGE_INSTALL} curl wget sudo qrencode procps iptables-persistent netfilter-persistent
 
-    wget -N https://raw.githubusercontent.com/inmetx/Hysteria-install/main/install_server.sh
+    wget -N https://raw.githubusercontent.com/inmetx/Hysteria2-install/main/install_server.sh
     bash install_server.sh
     rm -f install_server.sh
 
